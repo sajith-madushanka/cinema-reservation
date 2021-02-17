b6d46dd9fec7a2cdca7897b39efc3ddb2874986e
L.j.m.s.m.lankathilaka <74703614+sajith-madushanka@users.noreply.github.com>
2021-02-17 10:49:31 +0530

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Contact Us</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+</head>
+
+<body>
+<?php 
+        $link = mysqli_connect("localhost", "root", "", "cinema_db");
+?>
+    <header></header>
+    <div class="gmap_canvas"><iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5600.556044078955!2d81.05706338302384!3d6.98675020153185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae46216389fcf43%3A0x520e13e08fc68308!2sRex%20Theatre!5e0!3m2!1sen!2slk!4v1610627925888!5m2!1sen!2slk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    <div class="contact-us-container">
+        <div class="contact-us-section contact-us-section1">
+            <h1>Contact</h1>
+            <p>Contact us here </p>
+            <form action="" method="POST">
+                <input placeholder="First Name" name="fName" required><br>
+                <input placeholder="Last Name" name="lName" ><br>
+                <input placeholder="E-mail Address" name="eMail" required><br>
+                <textarea placeholder="Enter your message !" name="feedback" rows="10" cols="30" required></textarea><br>
+                <button type="submit" name="submit" value="submit">Send your Message</button>
+                <?php
+                    if(isset($_POST['submit'])){
+                        $insert_query = "INSERT INTO 
+                        feedbackTable ( senderfName,
+                                        senderlName,
+                                        sendereMail,
+                                        senderfeedback)
+                        VALUES (        '".$_POST["fName"]."',
+                                        '".$_POST["lName"]."',
+                                        '".$_POST["eMail"]."',
+                                        '".$_POST["feedback"]."')";
+                        mysqli_query($link,$insert_query);
+                        }
+                    ?>
+            </form>
+            
+        </div>
+        <div class="contact-us-section contact-us-section2">
+            <h1>Address & Info</h1>
+            <h3>Phone Numbers</h3>
+            <p><a href="tel:01011391148">+0555781816</a><br>
+                <a href="tel:01011391148">+0555687312</a></p>
+            <h3>Address</h3>
+            <p>Baily Road, Badulla</p>
+            <h3>E-mail</h3>
+            <p><a href="mailto:cinemareservation@bue.edu.eg">rextheator@gmail.com</a></p>
+        </div>
+    </div>
+    <footer></footer>
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/owl.carousel.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<div class="footer-section footer-section1">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-map-marked"></i> Reach Us</h2>
+    <div class="footer-section-inner-container">
+        <p></p>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>Provide by Sajith Madushanka </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a>Contact </a>
+</div>
